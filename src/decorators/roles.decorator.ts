8a981c838dda485cae6fa0f9b471c728ffe502bf
Roman Shevchenko <roman.shevchenko@kwiff.com>
2022-04-27 22:12:41 +0300

    init

--- src/decorators/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { Role } from '../database/schemas/users/roles.entity';
-export const ROLE_KEY = 'roles'
-
-export const Roles = (...args: Role[]) => SetMetadata(ROLE_KEY, args);
