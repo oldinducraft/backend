1043dd7f329587376df817b0e991a0aeb7fb33f7
Roman Shevchenko <kerch999@gmail.com>
2022-05-24 23:27:45 +0300

    soon

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Log } from './utils/logger/logging';
 import * as env from 'env-var';
+import { Database } from './utils/database/database';
 const log: Log = new Log();
 //TODO get from env var
 const httpPort = env.get('PORT').asIntPositive() || 3333;
@@ -8,6 +9,7 @@ const httpPort = env.get('PORT').asIntPositive() || 3333;
 //TODO set aup dotenv config
 class App {
   public express: express.Application;
+  private connectDatabase: Database;
 
   constructor(
     private log: Log,
@@ -15,9 +17,14 @@ class App {
   ) {
     this.express = express();
     this.options = options;
+
+    const database = new Database(this.log, 5_000, 30_000);
+    this.connectDatabase = database;
   }
 
-  // private mountDB() {}
+  public db(tableName: string) {
+    return this.connectDatabase.main(tableName);
+  }
 
   public startServer(): any {
     if (!!this.options.http) {
